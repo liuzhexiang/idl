8c36816106ae6a946cc5b2e7d9a2e23dd6d5fc9c
liuzhexiang <liuzhexiang@sina.cn>
2020-12-26 15:03:26 +0800

    add js generate

--- build.ps1
+++ build.ps1
@@ -1,11 +1,17 @@
 If (!(Test-Path .\ts)) {
     $null = New-Item -Path "$PSScriptRoot" -name "ts" -ItemType "directory"
 }
+If (!(Test-Path .\js)) {
+    $null = New-Item -Path "$PSScriptRoot" -name "js" -ItemType "directory"
+}
 Get-ChildItem .\proto -Filter *.proto | ForEach-Object -Process {
     $sourcePrefix = ".\proto\"
     protoc --go_out=. "$sourcePrefix$_"
     $prefix = ".\ts\"
+    $jsPrefix = ".\js\"
     $name = $_.BaseName
     $suffix = ".ts"
+    $jsSuffix = ".js"
     pbjs "$sourcePrefix$_" --ts "$prefix$name$suffix"
+    pbjs "$sourcePrefix$_" --es6 "$jsPrefix$name$jsSuffix"
 }
